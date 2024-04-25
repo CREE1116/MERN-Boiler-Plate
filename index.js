41055d3471bee6e708631e947ee8c18c6912749e
Lee_jongmin <60211689@mju.ac.kr>
2024-04-25 11:00:21 +0900

    환경변수 적용

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const mongoDBpassowrd = "KkQpmE91uWfy4OjT";
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const config = require("./config/key");
+
+//get user model
+const { User } = require("./model/user");
 
 //connect mongoDB
 mongoose
-  .connect(
-    `mongodb+srv://leejongmin774:${mongoDBpassowrd}@mern.zlp217k.mongodb.net/?retryWrites=true&w=majority&appName=MERN`,
-    {
-      useNewUrlParser: true,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+  })
   .then(() => {
     console.log("MongoDB Connected");
   })
@@ -18,8 +20,24 @@ mongoose
     console.log(err);
   });
 
+//middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+//routes
+//get
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+//post
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userData) => {
+    if (err) return res.json({ success: false, err });
+  });
+  return res.status(200).json({ success: true });
 });
 
 app.listen(5000, () => console.log("Server is running on port 5000"));
